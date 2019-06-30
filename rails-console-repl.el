dd914b7ef72ada0572bfbd870129b43e6d87b61e
takeokunn <bararararatty@gmail.com>
2019-06-30 13:48:51 +0900

    Refactor codes

--- rails-console-repl.el
+++ rails-console-repl.el
@@ -11,21 +11,24 @@
 
 ;;; Code:
 
-(require 'cc-mode)
 (require 'comint)
-(require 'thingatpt)
-(require 's)
 (require 'f)
+
+(defconst rails-console-repl-number "0.01"
+    "rails console repl version number.")
 
 (defgroup rails-console-repl nil
     "rails console repl"
     :tag "Rails"
     :prefix "rails-console-repl-"
-    :group 'ruby)
+    :group 'ruby
+    :link '(url-link :tag "repo" "https://github.com/takeokunn/rails-console-repl.el"))
 
-(defvar rails-console-repl-map
-    (let ((map (make-sparse-keymap)))
-        map))
+(defcustom rails-console-repl-exec-command '("bundle" "exec" "rails" "console")
+    "Rails console repl exec command"
+    :group 'rails-console-repl
+    :tag "Rails console repl execution command"
+    :type '())
 
 (defvar rails-console-repl-mode-hook nil
     "List of functions to be executed on entry to `rails-console-repl-mode'.")
@@ -37,10 +40,10 @@
 (defun rails-console-repl-run ()
     (interactive)
     (let ((buf-name "rails-console-repl")
-             (my-directory (read-directory-name "DIRECTORY: ")))
-        (setq default-directory my-directory)
+             (my-dir (read-directory-name "DIRECTORY: ")))
+        (setq default-directory my-dir)
         (switch-to-buffer
-            (apply 'make-comint-in-buffer buf-name my-directory  "bundle" nil '("exec" "rails" "c")))
+            (apply 'make-comint-in-buffer buf-name my-dir (car rails-console-repl-exec-command) nil (cdr rails-console-repl-exec-command)))
         (rails-console-repl-mode)
         (run-hooks 'rails-console-repl-hook)))
 

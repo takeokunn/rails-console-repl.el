04f919ea2e949aebfa005c9534f13e3ed8ae7f5c
takeokunn <bararararatty@gmail.com>
2019-06-30 14:54:54 +0900

    Update

--- rails-console-repl.el
+++ rails-console-repl.el
@@ -5,7 +5,6 @@
 ;; Author: takeokunn <bararararatty@gmail.com>
 ;; URL: https://github.com/takeokunn/rails-console-repl.el
 ;; Version: 0.01
-;; Package-Requires: ((comint-mode))
 
 ;; GPL v3
 
@@ -24,28 +23,61 @@
     :group 'ruby
     :link '(url-link :tag "repo" "https://github.com/takeokunn/rails-console-repl.el"))
 
+(define-derived-mode rails-console-repl-mode comint-mode "Rails Console"
+    "Major-mode for rails console REPL.")
+
+(defvar rails-console-repl-mode-map
+    (let ((map (make-sparse-keymap)))
+        map))
+
+(defvar rails-console-repl-mode-hook nil
+    "List of functions to be executed on entry to `rails-console-repl-mode'.")
+
 (defcustom rails-console-repl-exec-command '("bundle" "exec" "rails" "console")
     "Rails console repl exec command"
     :group 'rails-console-repl
     :tag "Rails console repl execution command"
     :type '())
 
-(defvar rails-console-repl-mode-hook nil
-    "List of functions to be executed on entry to `rails-console-repl-mode'.")
+(defvar rails-console-repl-comint-buffer-process
+    nil
+    "A list (buffer-name process) is arguments for `make-comint'.")
+(make-variable-buffer-local 'rails-console-repl-comint-buffer-process)
 
-(define-derived-mode rails-console-repl-mode comint-mode "Rails Console"
-    "Major-mode for rails console REPL.")
+(defun rails-console-repl--detect-buffer ()
+    "Return tuple list, comint buffer name and program."
+    (or rails-console-repl-comint-buffer-process
+        '("rails-console-repl" "rails-console-repl")))
+
+(defun rails-console-repl--make-process ()
+    (apply 'make-comint-in-buffer
+        (car (rails-console-repl--detect-buffer))
+        (cadr (rails-console-repl--detect-buffer))
+        (car rails-console-repl-exec-command)
+        nil
+        (cdr rails-console-repl-exec-command)))
 
 ;;;###autoload
-(defun rails-console-repl-run ()
+(defun rails-console-repl ()
     (interactive)
     (let ((buf-name "rails-console-repl")
              (my-dir (read-directory-name "DIRECTORY: ")))
         (setq default-directory my-dir)
-        (switch-to-buffer
-            (apply 'make-comint-in-buffer buf-name my-dir (car rails-console-repl-exec-command) nil (cdr rails-console-repl-exec-command)))
+        (switch-to-buffer (rails-console-repl--make-process))
         (rails-console-repl-mode)
         (run-hooks 'rails-console-repl-hook)))
+(put 'rails-console-repl 'interactive-only 'rails-console-repl-run)
+
+;;;###autoload
+(defun rails-console-repl-run (buf-name process)
+    (let ((rails-console-repl-comint-buffer-process (list buf-name process)))
+        (call-interactively 'rails-console-repl)))
+
+;;;###autoload
+(defun rails-console-repl-send-line ()
+    (let ((str (thing-at-point 'line 'no-properties)))
+        (comint-send-string (cadr (rails-console-repl--detect-buffer)) str)))
+
 
 (provide 'rails-console-repl)
 
